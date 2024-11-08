e31cec39003e1285f66a406a5164ffb1219a1bed
Noid <dionisiusgunadi@outlook.com>
2024-11-08 09:14:00 +0700

    completed exercises 03 part 01 to 05

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
 mod ticket {
-    struct Ticket {
-        title: String,
-        description: String,
-        status: String,
+    pub struct Ticket {
+        pub(crate) title: String,
+        pub(crate) description: String,
+        pub(crate) status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -38,8 +38,7 @@ mod ticket {
 mod tests {
     // TODO: Add the necessary `pub` modifiers in the parent module to remove the compiler
     //  errors about the use statement below.
-    use super::ticket::Ticket;
-
+    use crate::ticket::Ticket;
     // Be careful though! We don't want this function to compile after you have changed
     // visibility to make the use statement compile!
     // Once you have verified that it indeed doesn't compile, comment it out.
@@ -52,9 +51,6 @@ mod tests {
         //    |
         //    |              assert_eq!(ticket.description, "A description");
         //    |                         ^^^^^^^^^^^^^^^^^^
-        //
-        // TODO: Once you have verified that the below does not compile,
-        //   comment the line out to move on to the next exercise!
         assert_eq!(ticket.description, "A description");
     }
 
@@ -65,9 +61,6 @@ mod tests {
         //
         // This proves that `Ticket::new` is now the only way to get a `Ticket` instance.
         // It's impossible to create a ticket with an illegal title or description!
-        //
-        // TODO: Once you have verified that the below does not compile,
-        //   comment the lines out to move on to the next exercise!
         let ticket = Ticket {
             title: "A title".into(),
             description: "A description".into(),
